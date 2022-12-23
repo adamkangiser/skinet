b17f76bdb80589fe4a177a91b106598d35971b36
Adam Kangiser <109997201+adamkangiser@users.noreply.github.com>
2022-12-23 16:30:09 -0500

    10th Commit

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,20 +4,22 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { NgxPaginationModule } from 'ngx-pagination/public-api';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 
 @NgModule({
   declarations: [
     ShopComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
-    NgxPaginationModule
-  ],
-
-  exports: [ShopComponent]
+    NgxPaginationModule,
+    ShopRoutingModule
+  ]
 })
 export class ShopModule { }
