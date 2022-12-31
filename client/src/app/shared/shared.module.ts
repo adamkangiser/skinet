9b59b69fd48c7b64baf5d86af82b612ce427686d
Adam Kangiser <109997201+adamkangiser@users.noreply.github.com>
2022-12-31 17:37:50 -0500

    14th Commit

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CarouselModule } from 'ngx-bootstrap/carousel'
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { OrderTotalsComponent } from './order-totals/order-totals.component'
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OrderTotalsComponent
+  ],
   imports: [
     CommonModule,
     CarouselModule.forRoot()
   ],
   exports: [
     CommonModule,
-    CarouselModule
+    CarouselModule,
+    OrderTotalsComponent
   ]
 })
 export class SharedModule { }
